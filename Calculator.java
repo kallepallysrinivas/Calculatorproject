a61be2a9877ab37dc698644e91cfdd58a26dcdfe
kallepally srinivas <kallepallisrinivas1@gmail.com>
2024-10-22 15:54:35 +0530

    Pull requesting teacheeb by my dear frd ABHILKASH

--- Calculator.java
+++ Calculator.java
@@ -10,6 +10,6 @@ public class Calculator
 		Calculator cal=new calculator();
 		System.out.println("The sum of two numbers is"+(cal.add(2,3)));
 		System.out.println("srinivas is lkearning DEVOPS");
-		System.out.println("iull request teached by my dewar frd ABHILASH");
+		System.out.println("Pull request teached by my dear frd ABHILASH");
 	}
 }
